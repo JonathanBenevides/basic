8c9dde5652dc10e36e1aada2861fae7d52e55bf2
Benevides <jonathan.f.benevides@accenture.com>
2024-05-14 20:28:15 +0100

    switch

--- aula.js
+++ aula.js
@@ -1,15 +1,18 @@
 var result;
 
-let cond1 = 1;
-let cond2 = 2;
+// Cores
+// 1 vermelho
+// 2 azul
+// 3 preto
 
-// if (cond1 == 2) {
-    // result = 'condição é igual 1'
-// } else if (cond2 == 1) {
-    // result = 'condição é igual 2'
-// } else {
-    // result = 'condição não é nem 1 nem 2'
-// }
+const cor = 1;
+
+switch (cor) {
+    case '':
+        break;
+    default:
+        break;
+}
 
 console.log(result);
 document.querySelector('.title').textContent = result;
