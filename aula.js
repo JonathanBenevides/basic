1fa44e4df3a8e64d871b19f7ca2d1594e3df51da
Benevides <jonathan.f.benevides@accenture.com>
2024-05-21 20:33:59 +0100

    while

--- aula.js
+++ aula.js
@@ -1,13 +1,8 @@
-const n1 = 1;
-const n2 = 2;
-const name1 = 'Meu nome'
-const name2 = 'Meu sobrenome'
-const b1 = true;
-const b2 = false;
-
 let result = undefined;
 
-result = n1+n2;
+while (result) {
+    console.log(result)
+}
 
 console.log(result);
 document.querySelector('.title').textContent = result;
