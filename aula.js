ff2007a3520ecfbb4f66a969e8e2cc26d0053647
Benevides <jonathan.f.benevides@accenture.com>
2024-05-23 21:11:20 +0100

    a

--- aula.js
+++ aula.js
@@ -4,6 +4,17 @@ let result = undefined;
 // const a = true;
 // const b = true
 // result = a && b;
+// exercício
+// const felipe_eh_maior_de_idade = true
+// const vitor_eh_maior_de_idade = true
+// const sao_maiores = felipe_eh_maior_de_idade && vitor_eh_maior_de_idade
+// console.log(sao_maiores);
+
+
+// OR Operator - Este operador retorna verdadeiro quando um ao menos um valor de entrada é verdadeiro
+// const a = true;
+// const b = true
+// result = a || b;
 
 
 
